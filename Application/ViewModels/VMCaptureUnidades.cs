7dbcbb3132d988d56a89a408c30c9183d3adbc82
mulan <kbecerra774@gmail.com>
2025-02-17 12:14:02 -0600

    lectura de bascula configurada temporalmente para que solo lea 1, sin abrir puerto nina

--- Application/ViewModels/VMCaptureUnidades.cs
+++ Application/ViewModels/VMCaptureUnidades.cs
@@ -45,40 +45,42 @@ namespace Core.Application.ViewModels
 
         public string LeerPeso()
         {
-            //Configuracion para RHINO BAR-6x
-            SerialPort serialPort = new SerialPort(_basculaSettings.PuertoBascula, _basculaSettings.BaudRateBascula, Parity.None, _basculaSettings.DataBitsBascula, StopBits.One);
-            serialPort.Handshake = Handshake.None;
-            serialPort.WriteTimeout = 500;
-            serialPort.ReadTimeout = 500;
+            ////Configuracion para RHINO BAR-6x
+            //SerialPort serialPort = new SerialPort(_basculaSettings.PuertoBascula, _basculaSettings.BaudRateBascula, Parity.None, _basculaSettings.DataBitsBascula, StopBits.One);
+            //serialPort.Handshake = Handshake.None;
+            //serialPort.WriteTimeout = 500;
+            //serialPort.ReadTimeout = 500;
 
-            try
-            {
-                serialPort.Open();
+            //try
+            //{
+            //    serialPort.Open();
 
-                serialPort.Write(_basculaSettings.WriteCommandBascula);
+            //    serialPort.Write(_basculaSettings.WriteCommandBascula);
 
-                Thread.Sleep(500);
+            //    Thread.Sleep(500);
 
-                // Lee la respuesta de la báscula
-                string response = serialPort.ReadExisting();
-                if (response != null)
-                {
-                    response = response.Replace(_basculaSettings.SufijoBascula, "");
-                    return response.Trim();
-                }
-                throw new Exception("No se recibió respuesta de la báscula");
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Error al leer el peso de la báscula {ex.Message}", ex);
-            }
-            finally
-            {
-                if (serialPort.IsOpen)
-                {
-                    serialPort.Close();
-                }
-            }
+            //    // Lee la respuesta de la báscula
+            //    string response = serialPort.ReadExisting();
+            //    if (response != null)
+            //    {
+            //        response = response.Replace(_basculaSettings.SufijoBascula, "");
+            //        return response.Trim();
+            //    }
+            //    throw new Exception("No se recibió respuesta de la báscula");
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw new Exception($"Error al leer el peso de la báscula {ex.Message}", ex);
+            //}
+            //finally
+            //{
+            //    if (serialPort.IsOpen)
+            //    {
+            //        serialPort.Close();
+            //    }
+            //}
+
+            return "1";
         }
 
         public async Task UpdateMovimiento()
