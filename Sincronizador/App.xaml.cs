b8803e3c18d79d4a35ca445d7e3c17e8565cc7ed
mulan <kbecerra774@gmail.com>
2025-02-17 23:15:19 -0600

    WPF project added, ready to start working on it

---
+++ Sincronizador/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sincronizador;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ Sincronizador/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Sincronizador;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private bool isSyncing = false;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnSync_Click(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
