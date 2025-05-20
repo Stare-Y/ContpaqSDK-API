ff1e011927f7cb508723f2f957bfea1a7a43cbe5
pointasty <kbecerra774@gmail.com>
2025-05-20 12:58:57 -0600

    WEBSOCKET FIRST TEST WORKING !!!!

---
+++ BasculaTerminalApi/RandomNumberHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace BasculaTerminalApi
+{
+    public class RandomNumberHub : Hub
+    {
+        private static Timer _timer = null!;
+        private static IHubContext<RandomNumberHub> _context = null!;
+
+        public RandomNumberHub(IHubContext<RandomNumberHub> context)
+        {
+            _context = context;
+            if(_timer == null)
+            {
+                _timer = new Timer(3000);
+                _timer.Elapsed += SendRandomNumber;
+                _timer.Start();
+            }
+        }
+
+        private void SendRandomNumber(object sender, ElapsedEventArgs e)
+        {
+            var randomNumber = new Random().Next(1, 100);
+            _context.Clients.All.SendAsync("ReceiveNumber", randomNumber);
+        }
+    }
+}
---
+++ BasculaTerminalTest/BasculaClient.cs
@@ -0,0 +1,49 @@
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BasculaTerminalTest
+{
+    public class BasculaClient
+    {
+        private HubConnection _hubConnection = null!;
+        [Fact]
+        public async Task WebSocketTesting()
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5284/randomNumberHub")
+                .Build();
+
+            var receivedNumbers = new List<int>();
+
+            _hubConnection.On<int>("ReceiveNumber", (number) =>
+            {
+                receivedNumbers.Add(number);
+                Console.WriteLine($"Received Number: {number}");
+            });
+
+            await _hubConnection.StartAsync();
+
+            await Task.Delay(15000);
+
+            await _hubConnection.StopAsync();
+
+            Assert.NotEmpty(receivedNumbers);
+            foreach(var number in receivedNumbers)
+            {
+                Assert.InRange(number, 1, 100);
+            }
+        }
+            
+        private record NumberPayload(int Number);
+    }
+}
